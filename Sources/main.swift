73c0454b0ad43abacef6293f6e68f205c7131907
David Beck <code@thinkultimate.com>
2017-04-10 13:17:31 -0700

    Move main code into func

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,32 +7,33 @@
 //
 
 import Foundation
-import CoreData
 
 
-let optionPrefix = "--"
-
-
-if #available(macOS 10.12, *) {
-	let arguments = CommandLine.arguments.dropFirst() // first is always the path to the executable
-	let models = arguments.filter({ !$0.hasPrefix(optionPrefix) })
-	guard models.count > 0 else { fatalError("Missing model path") }
-	
-	let options = arguments.flatMap({ arg -> Option? in
-		guard let range = arg.range(of: "--"), range.lowerBound == arg.startIndex else { return nil }
-		return Option(rawValue: arg.substring(from: range.upperBound))
-	})
-	
-	cdmodeltoswift(modelPaths: models, options: options)
-		.then({ _ in
-			exit(0)
+public func main(arguments: [String]) {
+	if #available(macOS 10.12, *) {
+		let optionPrefix = "--"
+		
+		let arguments = arguments.dropFirst() // first is always the path to the executable
+		let models = arguments.filter({ !$0.hasPrefix(optionPrefix) })
+		guard models.count > 0 else { fatalError("Missing model path") }
+		
+		let options = arguments.flatMap({ arg -> Option? in
+			guard let range = arg.range(of: "--"), range.lowerBound == arg.startIndex else { return nil }
+			return Option(rawValue: arg.substring(from: range.upperBound))
 		})
-		.catch({ error in
-			exit(1)
-		})
-	
-	dispatchMain()
+		
+		cdmodeltoswift(modelPaths: models, options: options)
+			.then({ _ in
+				exit(0)
+			})
+			.catch({ error in
+				exit(1)
+			})
+		
+		dispatchMain()
+	} else {
+		fatalError("This program requires macOS 10.12 or greater.")
+	}
 }
 
-
-fatalError("This program requires macOS 10.12 or greater.")
+main(arguments: CommandLine.arguments)
